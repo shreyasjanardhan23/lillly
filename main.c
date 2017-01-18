4b85e61edc071f0966d526ff2a0b339c50010a1d
shreyasjanardhan23 <shreyasjanardhan23@gmail.com>
2017-01-18 00:21:16 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+struct student
+{
+     int id;
+     char name[30];
+     float percentage;
+};
+
+int main()
+{
+     int i;
+     struct student record1 = {1, "Raju", 90.5};
+     struct student *ptr;
+
+     ptr = &record1;
+
+         printf("Records of STUDENT1: \nn");
+         printf("  Id is: %d \n",ptr->id);
+         printf("  Name is: %s \n",ptr->name );
+         //printf("  Percentage is: %f n", *ptr);
+
+     return 0;
+}
